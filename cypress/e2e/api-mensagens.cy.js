23903d3384f5b364f1d7a7d1b0937693dd1c0cda
RAPHAEL BAQUETA BARBOSA <raphael.baqueta@globmail.com.br>
2024-06-08 15:25:58 -0300

    Stubs e massa de dados no fixture

--- cypress/e2e/api-mensagens.cy.js
+++ cypress/e2e/api-mensagens.cy.js
@@ -1,14 +1,11 @@
-describe('API Adopet', () => {
-    const authorization = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIyNTQxOWI1Yy1mNjg3LTQyZDktODAwOC1iNGI3MjczMGViMGUiLCJhZG9wdGVyTmFtZSI6IlJhcGhhZWwgQmFxdWV0YSIsImlhdCI6MTcxNzQ1MzU5MSwiZXhwIjoxNzE3NzEyNzkxfQ.h4-sRqSWKeFSQGPiiI_dfDGO1n3TkYxJQUzLlYmvFHo'
+describe('Api Adopet', () => {
+    const authorization = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMTY0M2NkNi03MTEyLTQxNWItOTVkMi0wNzkwNGIwZDFhMWMiLCJhZG9wdGVyTmFtZSI6IkFuYSBkZSBKZXN1cyIsImlhdCI6MTcwOTA0MTMwNSwiZXhwIjoxNzA5MzAwNTA1fQ.nhiaUyKdvN8RVxCkvne2gyI5n_pYnY_OMMdogMMqOlc`
 
-    it('Mensagens da API', () => {
+ it('Mensagens da API', () => {
         cy.request({
-            method: 'GET' ,
-            url: 'https://adopet-frontend-cypress.vercel.app/static/media/message-hover.585ef151de5fe106e8eb',
-            headers: { authorization }
-        }).then((res) => {
-            expect(res.status).to.be.equal(200)
-            expect(res.body).is.not.empty
-            expect(res.body).to.have.property('msg')
-    })
+         method: 'GET' ,
+         url: 'https://adopet-api-i8qu.onrender.com/mensagem/11643cd6-7112-415b-95d2-07904b0d1a1c',
+         headers: { authorization }
+        }) 
+ })
 })
