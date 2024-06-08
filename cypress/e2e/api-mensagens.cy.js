756163b6c4d7125b28948ee87653bb61638b1f68
RAPHAEL BAQUETA BARBOSA <raphael.baqueta@globmail.com.br>
2024-06-08 15:44:03 -0300

    Correção testes de API

--- cypress/e2e/api-mensagens.cy.js
+++ cypress/e2e/api-mensagens.cy.js
@@ -1,10 +1,10 @@
 describe('Api Adopet', () => {
-    const authorization = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMTY0M2NkNi03MTEyLTQxNWItOTVkMi0wNzkwNGIwZDFhMWMiLCJhZG9wdGVyTmFtZSI6IkFuYSBkZSBKZXN1cyIsImlhdCI6MTcwOTA0MTMwNSwiZXhwIjoxNzA5MzAwNTA1fQ.nhiaUyKdvN8RVxCkvne2gyI5n_pYnY_OMMdogMMqOlc`
+    const authorization = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIyNTQxOWI1Yy1mNjg3LTQyZDktODAwOC1iNGI3MjczMGViMGUiLCJhZG9wdGVyTmFtZSI6IlJhcGhhZWwgQmFxdWV0YSIsImlhdCI6MTcxNzg3MTIxMiwiZXhwIjoxNzE4MTMwNDEyfQ.7TOXi24aUpMw3_xjIkEa5VYWYlDcZJKgMaFRlQWTUjo`
 
  it('Mensagens da API', () => {
         cy.request({
          method: 'GET' ,
-         url: 'https://adopet-api-i8qu.onrender.com/mensagem/11643cd6-7112-415b-95d2-07904b0d1a1c',
+         url: 'https://adopet-api-i8qu.onrender.com/mensagem/25419b5c-f687-42d9-8008-b4b72730eb0e',
          headers: { authorization }
         }) 
  })
